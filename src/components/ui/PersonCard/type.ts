45005d424425ee4f0149fe030541095b23c7c7b1
Nastpavlova <Nastpavlovax@mail.ru>
2025-12-14 18:27:37 +0300

    fix: исправлены типы для PersonCardUI

---
+++ src/components/ui/PersonCard/type.ts
@@ -0,0 +1,19 @@
+type TSkillsPerson = {
+    teach: string[];
+    wantsToLearn: string[];
+};
+
+type TPerson = {
+    id: string;
+    img: string;
+    name: string;
+    city: string;
+    age: string;
+    skills: TSkillsPerson[];
+};
+
+export type TPersonCardUIProps = {
+    person: TPerson;
+    isLiked: string;
+    onLikeToggle: () => void;
+};
