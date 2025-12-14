6f67165d9c220223faa928a20c1670bec74b6de9
Grigory Morozov <morozehc@yandex.ru>
2025-12-14 17:29:08 +0300

    fix: убрал лишнюю логику с изменением padding из компонента ButtonUI

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -10,30 +10,17 @@ type TButtonUIProps = {
  onClick?:() => void,
 }
 
-type TCustomSettingBtn = {
-  horizontalPadding?: string,
-  verticalPadding?: string
-}
-
-export const ButtonUI: FC<TButtonUIProps & TCustomSettingBtn> = ({
+export const ButtonUI: FC<TButtonUIProps> = ({
   children, 
   color, 
   fulsSize = false, 
   disabledToggle = false, 
   onClick,   
-  horizontalPadding ='',
-  verticalPadding = ''
 }) => {
-  const styleHorizonPadding  = horizontalPadding ? {'--btn-padding-horizontal': horizontalPadding} : undefined;
-  const styleVerticalPadding  = verticalPadding ? {'--btn-padding-vertical': verticalPadding} : undefined;
-  const customStyle = {
-    ...styleHorizonPadding,
-    ...styleVerticalPadding 
-  } as React.CSSProperties
   const className = clsx(
     styles.button,
     color === 'primary' ? styles.buttonPrimary : styles.buttonSecondary,
     fulsSize ? styles.buttonFullSize : ''
   )
-  return <button style={customStyle} onClick={onClick} disabled={disabledToggle} className={className}>{children}</button>;
+  return <button onClick={onClick} disabled={disabledToggle} className={className}>{children}</button>;
 };
