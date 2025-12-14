48f21adeb2db01fc4225b474b2afc3877bcfa71c
Nastpavlova <Nastpavlovax@mail.ru>
2025-12-15 00:16:26 +0300

    feat: добавлен компонент PersonCard

--- src/components/PersonCard/PersonCard.tsx
+++ src/components/PersonCard/PersonCard.tsx
@@ -1,19 +1,27 @@
 import { useState, useEffect, useCallback } from "react";
-
-import { PersonCardUI } from "../ui/PersonCard/PersonCard";
+import { PersonCardUI } from "../ui/PersonCard/PersonCardUI";
 import type { TPersonCardUIProps } from '../ui/PersonCard/type';
+
+type TPersonCardProps = Omit<TPersonCardUIProps, 'isLiked'> & {
+    isLiked?: string; // id пользователя для проверки в localStorage
+};
 
 export const PersonCard = ({
     person,
     isLiked,
     onLikeToggle,
-}: TPersonCardUIProps) => {
+    onClickCardButton
+}: TPersonCardProps) => {
 
-    const [isButtonLiked, setIsButtonLiked] = useState(isLiked);
+    //заглушка для isLiked (?)
+    const dummy = isLiked? true : false;
+
+    const [isButtonLiked, setIsButtonLiked] = useState(dummy);
 
     useEffect(() => {
-        setIsButtonLiked(isLiked);
-    }, [isLiked]);
+        // Логика проверки localStorage будет здесь
+        setIsButtonLiked(dummy);
+    }, [dummy]);
 
     const handleLikeToggle = useCallback(() => {
         setIsButtonLiked(prev => !prev);
@@ -25,6 +33,7 @@ export const PersonCard = ({
             person={person}
             isLiked={isButtonLiked}
             onLikeToggle={handleLikeToggle}
+            onClickCardButton={onClickCardButton}
         />
     );
 };
