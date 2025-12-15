39d4de3c5925bb29e4f89e586d870e29c4c7121d
Nastpavlova <Nastpavlovax@mail.ru>
2025-12-16 00:35:03 +0300

    fix: изменила моки под новые типы

--- src/components/PersonCard/mocks/personMocks.ts
+++ src/components/PersonCard/mocks/personMocks.ts
@@ -1,29 +1,46 @@
 import type { TPerson } from '../../ui/PersonCard/type';
 
 export const mockPerson1: TPerson = {
-    id: '11111111-1111-1111-1111-111111111111',
-    img: 'https://commons.wikimedia.org/wiki/Special:FilePath/A_drummer.jpg?width=200',
-    name: 'Иван Ковалёв',
-    city: 'Санкт-Петербург',
-    age: '34 года',
-    skills: [
-        {
-        teach: ['Игра на барабанах'],
-        wantsToLearn: ['Тайм менеджмент', 'Медитация', 'Йога'],
-        },
+    "id": 1,
+    "avatarUrl": "https://commons.wikimedia.org/wiki/Special:FilePath/A_drummer.jpg?width=200",
+    "name": "Алексей Петров",
+    "location": "Санкт-Петербург",
+    "age": "34 года",
+    "gender": "Мужской",
+    "skillCanTeach": {
+        "id": 101,
+        "name": "Фотография",
+        "description": "Основы съемки, работа с камерой и обработка фото."
+    },
+    "images": [
+        "https://picsum.photos/seed/101/400/300",
+        "https://picsum.photos/seed/102/400/300"
     ],
+    "subcategoriesWantToLearn": [
+        { "id": 201, "name": "Рисование" },
+        { "id": 202, "name": "Музыка" },
+        { "id": 204, "name": "Программирование" }
+    ]
 };
 
 export const mockPerson2: TPerson = {
-    id: '22222222-2222-2222-2222-222222222222',
-    img: 'https://commons.wikimedia.org/wiki/Special:FilePath/Artist_painting_watercolor_%28Unsplash%29.jpg?width=200',
-    name: 'Анна Смирнова',
-    city: 'Санкт-Петербург',
-    age: '35 лет',
-    skills: [
-        {
-        teach: ['Рисование акварелью'],
-        wantsToLearn: ['Видеомонтаж', 'Йога'],
-        },
+    "id": 2,
+    "avatarUrl": "https://commons.wikimedia.org/wiki/Special:FilePath/Artist_painting_watercolor_%28Unsplash%29.jpg?width=200",
+    "name": "Марина Соколова",
+    "location": "Санкт-Петербург",
+    "age": "32 года",
+    "gender": "Женский",
+    "skillCanTeach": {
+        "id": 102,
+        "name": "Веб-дизайн",
+        "description": "Создание макетов сайтов, UI/UX дизайн и прототипирование."
+    },
+    "images": [
+        "https://picsum.photos/seed/103/400/300",
+        "https://picsum.photos/seed/104/400/300"
     ],
+    "subcategoriesWantToLearn": [
+        { "id": 203, "name": "Фотография" },
+        { "id": 204, "name": "Программирование" }
+    ]
 };
