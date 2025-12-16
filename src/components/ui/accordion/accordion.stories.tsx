0d93043f64982fd54a0f8ec2b62bb27edb25eaf2
Grigory Morozov <morozehc@yandex.ru>
2025-12-17 01:35:44 +0300

    feat: добавлен сторис для компонента AccordionUI

--- src/components/ui/accordion/accordion.stories.tsx
+++ src/components/ui/accordion/accordion.stories.tsx
@@ -1,73 +1,38 @@
-// import type { Meta, StoryObj } from "@storybook/react";
-// import { AccordionUI } from "./accordion";
-
+import type { Meta, StoryObj } from "@storybook/react";
+import { AccordionUI } from "./accordion";
+import { languages } from "./mocks/checkboxs";
 // // Метаданные — настройки для Storybook
-// const meta: Meta<typeof AccordionUI> = {
-// title: "Components/ui/AccordionUI", // название в боковом меню Storybook
-// component: AccordionUI,
-// parameters: {
-// layout: "centered", // По умолчанию компоненты центрируются
-// },
-// argTypes: {
-// onChange: { action: "clicked" }, // Пример событий
-// variant: {
-//   control: 'radio', // формат переключения
-//   options: ['square', 'round', 'accordion']
-// },
-// isActive: {control: 'boolean'},
+const meta: Meta<typeof AccordionUI> = {
+  title: "Components/ui/AccordionUI", // название в боковом меню Storybook
+  component: AccordionUI,
+  parameters: {
+  layout: "centered", // По умолчанию компоненты центрируются
+  },
+  argTypes: {
+    onChange: { action: "clicked" }, // Пример событий
+    onItemChange: {action: 'clicked'},
+    isActive: {control: 'boolean'},
+    items: { control: "object" },
+    label: { control: 'text' },
 
-// label: { control: 'text' },
-// groupName: { control: 'text'}
-// },
-// };
-// export default meta;
+  },
+};
+export default meta;
 
 // // Создаём тип для истории - гарантия соотв пропсам комп-та
-// type Story =StoryObj<typeof AccordionUI>;
-// //  дефолт
-// export const story: Story = {
-//   args: {
-//     label: 'Английский язык',
-//     variant: 'square',
-//     isActive: false,
-//     groupName: 'Иностранные языки'
-//   }
-// };
+type Story =StoryObj<typeof AccordionUI>;
+//  дефолт
+export const square: Story = {
+  render: (args) => {
+    return (<div style={{ width: 300, display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+      <AccordionUI {...args}/>
+    </div>)
+  },
+  args: {
+    label: 'Иностранные языки',
+    isActive: false,
+    items: languages
+  }
+};
 
-// // //  секондари
-// // export const Secondary: Story = {
-// //   args: {
-// //     color: 'secondary',
-// //     fulsSize: false,
-// //     disabledToggle: false,
-// //     children: 'Обмен предложен'
-// //   }
-// // }
-
-// // // disabled
-
-// // export const Disabled: Story = {
-// //   args: {
-// //     color: 'secondary',
-// //     fulsSize: false,
-// //     disabledToggle: true,
-// //     children: 'Сохранить Disabled'
-// //   }
-// // }
-
-// // // fullWidth
-
-// // export const fullWidth: Story = {
-// //   render: (args) => (
-// //     <div style = {{width: '400px', padding: '20px', border: '2px solid black'}}>
-// //       <CheckBoxUI {...args}/>
-// //     </div>
-// //   ),
-// //   args: {
-// //     color: 'primary',
-// //     fulsSize: true,
-// //     disabledToggle: false,
-// //     children: 'Предложить обмен'
-// //   }
-// // }
 
