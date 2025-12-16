4612a8291ebe766d3b3e6ccccfedf64a7d8129be
Grigory Morozov <morozehc@yandex.ru>
2025-12-17 01:40:18 +0300

    feat: убрана заглушка у компонента CheckboxUI, вместо нее добавлен рабочий компонент IconButton

--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -4,37 +4,16 @@ import checkedSquare from './assets/images/checkedSquare.svg';
 import emptyRound from './assets/images/emptyRoundCheckBox.svg';
 import checkedRound from './assets/images/checkedRound.svg';
 import mainAccordion from './assets/images/mainChecboxSquare.svg';
+import { IconButtonUI } from '../IconButton/IconButtonUI'; 
 import styles from './checkbox.module.css';
 
-type IconBtnProps = {
-icon:  string;
-iconActive?:  string;
-isActive?: boolean;
-onClick?: () => void;
-}
-
-const IconButton: FC<IconBtnProps> = (
-  {icon,
-  iconActive,
-  isActive = true,
-  onClick
-}
-) => {
-      const src = isActive && iconActive ? iconActive : icon;
- return (
-      
-    <button  onClick={onClick}>
-      <img src={src} alt="Значок чекбокса" />
-    </button>
-
-  )
-}
 export type CheckboxProps = {
   label: string; //- текст
   isActive: boolean; //- состояние активности передающееся в IconButton
-  onChange?: (id: string) => void; //- функция для изменения состояния передающаяся в IconButton
+  onChange: (id: string) => void | (() => void); //- функция для изменения состояния передающаяся в IconButton
   variant?: 'square' | 'round' | 'accordion';
-  groupName?: string
+  groupName?: string,
+  id: string
 };
 
 export const CheckBoxUI: FC<CheckboxProps> = ({
@@ -42,7 +21,8 @@ export const CheckBoxUI: FC<CheckboxProps> = ({
   isActive,
   onChange,
   variant ='square',
-  groupName
+  groupName,
+  id
 }) => {
      const checkBoxData = {
         empty:'',
@@ -69,9 +49,9 @@ export const CheckBoxUI: FC<CheckboxProps> = ({
              name={groupName}
              value= {label}
              className={`${styles.visuallyHidden} ${styles.checkbox}`}
-             onChange={() =>onChange}
+             onChange={() => onChange(id)}
        />
-        <IconButton isActive={isActive} icon={checkBoxData.empty} iconActive={checkBoxData.checked} />
+        <IconButtonUI isActive={isActive} icon={checkBoxData.empty} iconActive={checkBoxData.checked} />
 
         <span className ={styles.checkboxName}>{label}</span>
       </label>
