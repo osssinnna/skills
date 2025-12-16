bf9bdaacdd525ca869f44f166ad0c05d76c05750
Grigory Morozov <morozehc@yandex.ru>
2025-12-17 01:42:38 +0300

    feat: убраны тестовые моки из файла компонента AccordionUI и добавлен обработчик появления выпадающего списка

--- src/components/ui/accordion/accordion.tsx
+++ src/components/ui/accordion/accordion.tsx
@@ -1,22 +1,27 @@
-import {  type FC } from 'react';
+import {  useState, type FC } from 'react';
 import styles from './accordion.module.css';
 import {CheckBoxUI} from '../checkbox/index';
-import type {CheckboxProps} from '../checkbox/checkbox'
+import opened from '../accordion/assets/images/opened.svg';
+import closed from '../accordion/assets/images/closed.svg'
+import { IconButtonUI } from '../IconButton/IconButtonUI';
+import clsx from 'clsx';
+
+type TItemChecbox = {
+  id: string,
+  label: string,
+  isActive: boolean
+}
 
 type AccordionCheckboxProps = {
   label: string; //текст аккордеона
   isActive: boolean; //активность галочки самого аккордиона
   onChange: () => void; // для выбора всех под чекбоксов
-  items: CheckboxProps[]; //чекбоксы внутри аккордеона
+  items: TItemChecbox[]; //чекбоксы внутри аккордеона
   onItemChange: (id: string) => void;  // обработчик изменения состояния внутреннего чекбокса
 };
 
 
-const languages = [
-  { id: 1, label: 'Английский', isActive: true },
-  { id: 2, label: 'Французский', isActive: false },
-  { id: 3, label: 'Немецкий', isActive: false },
-];
+
 
 export const AccordionUI: FC<AccordionCheckboxProps> = ({
   label,
@@ -25,24 +30,24 @@ export const AccordionUI: FC<AccordionCheckboxProps> = ({
   items,
   onItemChange,
 }) => {
-     
-     
+     const [isOpened, toggleItems] = useState(false);
+     const handleSetItems = () => {
+      toggleItems(!isOpened)
+     }
   return (
-    <>
-      <CheckBoxUI label={label} variant={'accordion'} isActive ={isActive} groupName={label} onChange={onChange}/>
-      <ul>
-        
+    <div className = {styles.accordion}>
+    <div className = {styles.accordionPanel}>
+      <CheckBoxUI aria-expanded={isActive} label={label} variant={'accordion'} isActive ={isActive} groupName={label} onChange={onChange} id={'_'}/>
+      <IconButtonUI onClick={handleSetItems} isActive={isOpened} icon={closed} iconActive={opened}/>
+    </div>
+      <ul className={clsx( styles.accordionContent, isOpened && styles.accordionContentOpen)} aria-hidden={!isOpened}>
         {items.map((item) => (
-          <li key={item.label}>
-            <CheckBoxUI {...item} onChange={onItemChange}/>
+          <li className={styles.accordionItem} key={item.id}>
+            <CheckBoxUI {...item} onChange={()=>onItemChange(item.id)} />
           </li>
         ))}
-
-        
-
-
       </ul>
-    </>
+    </div>
   )
 };
 
