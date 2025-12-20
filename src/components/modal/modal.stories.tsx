3197b553da4e6c612d019bbb26c9eceda81a0870
Grigory Morozov <morozehc@yandex.ru>
2025-12-20 11:57:34 +0300

    feat: компонент Modal -добавлено действие при нажатии на кнопку в модалке - переход на 404- пока только для стори

--- src/components/modal/modal.stories.tsx
+++ src/components/modal/modal.stories.tsx
@@ -4,7 +4,8 @@ import { ModalMessageUI } from "../ui/modal/modal-message";
 import bellIcon from '../../assets/modal-message/notificationBell-icon.svg';
 import { ButtonUI } from "../ui/button";
 import { useGoBack } from "../../utils/hooks";
-import { Link, MemoryRouter, Route, Routes, useLocation } from "react-router-dom";
+import { Link, MemoryRouter, Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import { NotFound404 } from "../../pages/not-found-404/not-found-404";
 
 const meta: Meta<typeof Modal> = {
   title: "Components/Modal",
@@ -22,6 +23,10 @@ type Story = StoryObj<typeof Modal>;
 const ModalStory = () => {
 
   const goBack = useGoBack(1);
+  const navigate = useNavigate();
+  const handleClose = () => {
+      navigate('/404');
+  }
   return (
     <>
       <Modal onClose={()=> goBack()}>
@@ -29,7 +34,7 @@ const ModalStory = () => {
             image={bellIcon}
             title="Вы предложили обмен"
             text="Проверка слушателей закрытия модалки по Esc и оверлей"
-            buttonsProps={[{ textBtn: 'Готово', statusBtn: 'primary' }]}
+            buttonsProps={[{ textBtn: 'Готово', statusBtn: 'primary', onClick: handleClose}]}
           />
         </Modal>
       
@@ -51,6 +56,7 @@ const PageHome = () => {
 const AppRoutes = () => {
     const location = useLocation();
     const backgroundLocation = location.state?.background;
+    const goBack = useGoBack(1);
     return <>
               <Routes location={backgroundLocation || location}>
                 {/* Главная страница с кнопкой */}
@@ -58,6 +64,12 @@ const AppRoutes = () => {
                   path="/"
                   element={
                     <PageHome/>
+                  }
+                />
+                <Route
+                  path="/404"
+                  element={
+                    <NotFound404 />
                   }
                 />
               </Routes>
