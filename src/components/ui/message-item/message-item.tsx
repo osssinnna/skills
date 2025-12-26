9bdb93700704bbfcdda91afd246e4d3d5330b54e
Grigory Morozov <morozehc@yandex.ru>
2025-12-26 10:50:59 +0300

    feat: создан компонент messageItemUI

---
+++ src/components/ui/message-item/message-item.tsx
@@ -0,0 +1,48 @@
+import type { FC } from 'react';
+import { ButtonUI } from '../button';
+import styles from './message-item.module.css';
+
+type TMessage = {
+  title: string;
+  comment: string
+}
+type TMessagePropsUI = {
+  userName: string;
+  userId: number; // - для перехода на карточку пользователя и отмечания соо как прочитаннок
+  date: string; // - дата создания сообщения
+  viewed: boolean; // - просмотрено ли сообщение
+  onView?: () => void;
+  image?: string;
+  message: TMessage;
+}
+
+export const MessageItemUI:FC<TMessagePropsUI> = ({
+  userName,
+  userId,
+  date,
+  viewed,
+  onView,
+  image,
+  message,
+
+  }) => {
+   
+  return (
+    <div className={styles.messageItem} key={String(userId)}>
+      <div className={styles.description}>
+        <div className={styles.main}>
+          <img className={styles.icon} src={image} alt="иконка сообщения о результате предложения" />
+          <div className={styles.text}>
+            <h3 className={styles.title}>{`${userName} ${message.title}`}</h3>
+            <p className={styles.comment}>{message.comment}</p>
+          </div>
+        </div>
+        <div className={styles.date}>{date}</div>
+      </div>
+      
+      {!viewed && (
+        <ButtonUI onClick={onView} color={'primary'}>Перейти</ButtonUI>
+      )}
+    </div>
+  )
+}
