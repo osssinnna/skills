4f697a02c2db5ff1d06590003d4b2fa4d47eff16
Grigory Morozov <morozehc@yandex.ru>
2025-12-26 14:57:59 +0300

    refactor: оптимизация типов пропса к-та MessageItem - добавлен тип TMessageNotiFying

--- src/components/message-item/message-item.tsx
+++ src/components/message-item/message-item.tsx
@@ -3,17 +3,15 @@ import { MessageItemUI } from '../ui/message-item';
 import iconMessage from '../../assets/message-item/icon-message.svg'
 import { useDispatch } from '../../services/store';
 import { useNavigate } from "react-router-dom";
-
+import type { TMessageExhanges ,TMessageNotifying } from '../../utils/types';
 import { calcData } from '../../utils/calc-data';
+export type TMessageProps = TMessageNotifying;
 
-type TMessageExhanges = 'confirmed' | 'offered' | 'rejected';
-
-export type TMessageProps = {
-  userName: string;
-  userId: number; // - для перехода на карточку пользователя и отмечания соо как прочитаннок
-  date: string; // - дата создания сообщения
-  viewed: boolean; // - просмотрено ли сообщение
-  typeMessage: TMessageExhanges;
+ //  определяем содержание сообщения уведомления исходя из 'confirmed' | 'offered' | 'rejected';
+const valuesMessages: TValuesMessages = {
+    confirmed: { title: 'принял ваш обмен', comment: 'Перейдите в профиль, чтобы обсудить детали'},
+    offered: { title: 'предлагает вам обмен', comment: 'Примите обмен, чтобы обсудить детали'},
+    rejected: { title: 'отклонил ваш обмен', comment: 'Нажмите подробнее,чтобы посмотреть детали'}, // возможно пригодится и такой вариант
 }
 
 type TValuesMessages = Record<TMessageExhanges, {title: string, comment: string}>
@@ -28,7 +26,7 @@ export const MessageItem:FC<TMessageProps> = ({
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    //  переход чтобы посмотреь предложение обмена
+    //  переход чтобы посмотреь именно новое непросмотренное предложение обмена
     const handleToViewClick =  () => {
         // dispatch(markAsViewed(message.userId)) //  => расскоментировать когда будет рабоч слайс messagesSlice
         navigate('/404'); // пока заглушка
@@ -36,13 +34,7 @@ export const MessageItem:FC<TMessageProps> = ({
 
     //  форматируем дату
     const formatDate = calcData(date);
-    //  определяем содержание сообщения уведомления исходя из 'confirmed' | 'offered' | 'rejected';
-    const valuesMessages: TValuesMessages = {
-        confirmed: { title: 'принял ваш обмен', comment: 'Перейдите в профиль, чтобы обсудить детали'},
-        offered: { title: 'предлагает вам обмен', comment: 'Примите обмен, чтобы обсудить детали'},
-        rejected: { title: 'отклонил ваш обмен', comment: 'Нажмите подробнее,чтобы посмотреть детали'}, // возможно пригодится и такой вариант
-    }
-    
+    //  определили содержание уведомления
     const message = valuesMessages[typeMessage];
 
     return (
