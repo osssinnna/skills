285e5b18fb05a4c2277f4446041590d33e26bf80
Grigory Morozov <morozehc@yandex.ru>
2025-12-26 14:56:04 +0300

    fix: к-т MessagesList убраны ненужные импорты стилей, и  ненужный обработчик в пропсах

--- src/components/messages-list/messages-list.tsx
+++ src/components/messages-list/messages-list.tsx
@@ -1,21 +1,17 @@
 import type { FC } from 'react';
-import styles from './messages-list.module.css';
-import { useDispatch } from '../../../services/store';
+import { useDispatch } from '../../services/store';
 import { MessagesListUI } from '../ui/messages-list/messages-list';
 import type { TMessageProps } from '../message-item/message-item';
 
 export type TMessagesListProps = {
   title: string,
   messages: TMessageProps[],
-  onClick: () => void
 }
 
 
 export const MessagesList:FC<TMessagesListProps> = ({
   title,
   messages,
-  //  проблемас датой ??  https://chatgpt.com/c/694d3875-4a4c-8331-bed9-7e78e0f5e806
-  //  прочитать все либо очистить  функция обработчик
   //  если нету -рендерим что ничего нет
   }) => {
     const dispatch = useDispatch();
@@ -27,8 +23,16 @@ export const MessagesList:FC<TMessagesListProps> = ({
     const handleMessagesClick = () => {
       if(isViewedAllMessages) {
         // очистить недавно просмотренные
+        messages.forEach((message) => {
+          // dispatch(clerRecentlyViewed(message.userId))
+        })
       } else {
         //  отмечаем новые как просмотренные
+        messages.forEach((message) => {
+          // dispatch(markAsViewed(message.userId))
+
+        })
+
       }
     }
     return <MessagesListUI title={title} messages={messages} onClick={handleMessagesClick} textButton={textForBtn}/>
