bdda8ebcdcc3353b40d69f4a1f5dcc5f7ea0e3ca
Grigory Morozov <morozehc@yandex.ru>
2025-12-27 18:07:06 +0300

    feat: добавлены атрибуты доступности для компонента MessagesSectionUI

--- src/components/ui/messages-section/messages-section.tsx
+++ src/components/ui/messages-section/messages-section.tsx
@@ -16,14 +16,28 @@ type TMessagesSectionUIProps = {
 export const MessagesSectionUI = forwardRef<HTMLElement, TMessagesSectionUIProps>(({unreadMessages, readMessages, isVisible}, ref) => {
     const hasMessages = unreadMessages.length > 0 || readMessages.length > 0;
   return (
-    <section ref={ref} className={clsx(styles.messagesListSection, isVisible ? styles.messagesListSectionOpen : '' )}>
+    <section aria-hidden={!isVisible} aria-label={'Уведомления о предложениях'} ref={ref} className={clsx(styles.messagesListSection, isVisible ? styles.messagesListSectionOpen : '' )}>
       {hasMessages ? (
-      <>
-        <MessagesList title="Новые уведомления" messages={unreadMessages} />
-        <MessagesList title="Просмотренные" messages={readMessages} />
+      <> 
+        {
+          unreadMessages.length > 0 && (
+            <MessagesList title="Новые уведомления" messages={unreadMessages} />
+          )
+        }
+        {
+          readMessages.length > 0 && (
+            <MessagesList title="Просмотренные" messages={readMessages} />
+          )
+        }
       </>
       
-      ) : 'уведомлений  о новых предложениях пока нет'
+      ) : 
+      <p className={styles.emptyState}
+        role='status'
+      >
+        уведомлений  о новых предложениях пока нет
+      </p>
+  
     }
     </section>
   )
