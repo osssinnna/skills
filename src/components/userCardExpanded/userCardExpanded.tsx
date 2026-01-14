1030a8ca89033999e8e5a49dc7d4125bc1cdf978
boshan <ziijey@yandex.ru>
2026-01-15 02:11:42 +0300

    свадьба расширеной карточки и похожих предложений

--- src/components/userCardExpanded/userCardExpanded.tsx
+++ src/components/userCardExpanded/userCardExpanded.tsx
@@ -3,11 +3,13 @@ import { useNavigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import { UserCardExpandedUI } from "../ui/userCardExpanded/userCardExpandedUI";
+import { SimilarCardsSection } from "../similar-cards-section/similar-cards-section";
 import type { TUserCardExpandedUIProps } from "../ui/userCardExpanded/types";
 
 import { usePersonLike } from "../../hooks/usePersonLike";
 import { selectCurrentUser } from "../../services/currentUserSlice/selectors";
 import { useExchangeUsers } from "../../hooks/useExchangeUsers";
+import { useSimilarUsers } from "../../hooks/useSimilarUsers";
 
 type TUserCardExpandedProps = Omit<
   TUserCardExpandedUIProps,
@@ -36,20 +38,34 @@ export const UserCardExpanded = ({
 
   const handleExchangeClick = useCallback(() => {
     if (currentUser === null) {
-      // Перенаправление на регистрацию с сохранением текущей страницы
       navigate("/register", { state: { from: location } });
     } else {
       toggleExchange(user.id);
     }
-  }, [currentUser, navigate, location]);
+  }, [currentUser, navigate, location, toggleExchange, user.id]);
+
+  // Получаем похожих пользователей 
+  const similarUsers = useSimilarUsers(user.id, 8);
+
+  // Если нет похожих пользователей, не показываем 
+  const hasSimilarUsers = similarUsers.length > 0;
 
   return (
-    <UserCardExpandedUI
-      user={user}
-      isLiked={isLiked}
-      isExcahnged={hasExchange(user.id)}
-      onLikeToggle={handleLikeToggle}
-      onExchangeClick={handleExchangeClick}
-    />
+    <>
+      <UserCardExpandedUI
+        user={user}
+        isLiked={isLiked}
+        isExcahnged={hasExchange(user.id)}
+        onLikeToggle={handleLikeToggle}
+        onExchangeClick={handleExchangeClick}
+      />
+      
+      {hasSimilarUsers && (
+        <SimilarCardsSection
+          title="Похожие пользователи"
+          users={similarUsers}
+        />
+      )}
+    </>
   );
 };
